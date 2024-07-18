bd4352bcae56362c77ef9e9760cafb881370ae42
Vinicius Gasparello da Silva <61200874+vgdsilva@users.noreply.github.com>
2024-07-18 15:23:00 -0300

    Add TabBar Pages

---
+++ TimeTracker.Mobile/Views/Pages/RecordingHours/RecordingHourPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TimeTracker.Mobile.Views.Pages.RecordingHours;
+
+public partial class RecordingHourPage : ContentPage
+{
+	public RecordingHourPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TimeTracker.Mobile/Views/Pages/Settings/SettingPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TimeTracker.Mobile.Views.Pages.Settings;
+
+public partial class SettingPage : ContentPage
+{
+	public SettingPage()
+	{
+		InitializeComponent();
+	}
+}
